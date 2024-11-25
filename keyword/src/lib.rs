3be6ed0dc64f137904a2bea2df135df63603a1b2
confucuis <confucuis@github.com>
2024-11-25 16:33:50 +0800

    ..

--- keyword/src/lib.rs
+++ keyword/src/lib.rs
@@ -1,26 +1,27 @@
 /// 关键字示例
-use mlib;
+pub mod rtrait;
 
 // 模块mod
-mod m_module {
+pub mod m_module {
     // 对外不可见
     fn example_a() {
-        println!("hello world");
+        println!("a: hello world");
     }
 
     // 对外可以见
     pub fn example_b() {
-        println!("hello world");
+        example_a();
+        println!("b: hello world");
     }
 
     // crate之内可见,crate之外不可见
     pub(crate) fn example_c() {
-        println!("hello world");
+        println!("c: hello world");
     }
 
     // 只能在当前模块的父模块可见
     pub(super) fn example_d() {
-        println!("hello world");
+        println!("d: hello world");
     }
 }
 
--- keyword/src/rtrait/demo.rs
+++ keyword/src/rtrait/demo.rs
@@ -1,3 +1,4 @@
+// - 简单的示例
 // 声明接口
 pub trait Behavior {
     fn run(&self);
@@ -23,5 +24,56 @@ impl<'a> Behavior for Dog<'a> {
 
     fn swim(&self) {
         println!("{} swimming ...", self.name);
+    }
+}
+
+// - trait中关联type类型占位符
+pub trait Sing {
+    type Person;
+    fn speak(&self);
+}
+
+pub struct Chinese {
+    pub name: String,
+}
+
+impl Sing for Chinese {
+    type Person = String;
+
+    fn speak(&self) {
+        println!("i am chinese.");
+    }
+}
+
+pub struct American {
+    pub name: String,
+}
+
+impl Sing for American {
+    type Person = String;
+
+    fn speak(&self) {
+        println!("i am american.")
+    }
+}
+
+impl Chinese {
+    pub fn new() -> Self {
+        Chinese{name: "张三".parse().unwrap() }
+    }
+}
+
+impl American {
+    pub fn new() -> Self {
+        American{name: "卢卡斯".parse().unwrap() }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_mod() {
+        crate::m_module::example_c();
+        crate::m_module::example_d();
     }
 }
